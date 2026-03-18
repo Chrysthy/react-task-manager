a41131b34d6a03b282c836758ac2bcf0b2e15c00
Chrystine Martins <chrystine.martins@outlook.com>
2026-03-17 21:54:21 -0300

    refacto: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <>
-
-      <p className="text-[#6528d3] text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam et enim atque, exercitationem aut voluptates accusantium ex mollitia? Tempora aperiam itaque odit perferendis eligendi nesciunt magni culpa laudantium modi omnis.</p>
-
+      
     </>
   )
 }
