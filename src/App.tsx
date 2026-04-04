5d307f382238efec0e062aead37852e232d6f588
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-03 22:13:17 -0300

    feat: reorganizar estrutura do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-
+import TodoForm from "./components/TodoForm";
+import TodoHeader from "./components/TodoHeader";
 
 function App() {
 
   return (
     <>
-      
+      <main className="bg-dark-bg h-screen">
+
+        <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+
+          <div className="max-w-175 m-auto p-2">
+
+            <TodoHeader />
+
+            <TodoForm/>
+
+          </div>
+
+        </div>
+
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
