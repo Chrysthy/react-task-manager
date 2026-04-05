c09eb918f956e170d334c0724ad8e659576ce802
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-04 21:34:31 -0300

    fix: ajustar estilos e classes no componente TodoForm

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,13 +1,22 @@
-const TodoForm = () => {
-    return (
-        <form className="relative mb-10">
+import { useContext } from "react";
+import { themeConfig } from '../../contexts/theme';
+import { ThemeContext } from '../../contexts/ThemeContext';
 
-            <span className="absolute w-6 h-6 border border-dark-border top-1/2 transform -translate-y-1/2 rounded-full left-6"></span>
+const TodoForm = () => {
+
+    const { theme } = useContext(ThemeContext);
+    const config = themeConfig[theme].todo;
+
+    return (
+        <form className="relative mb-10 w-full block">
+
+            <span className={`absolute w-6 h-6 border ${config.border} top-1/2 transform -translate-y-1/2 rounded-full left-6`}></span>
 
             <input
                 type="text"
                 name="todo"
-                placeholder="Create a new todo..." className="bg-dark-surface w-full text-white rounded-md py-6 pl-16 outline-none text-lg " />
+                placeholder="Create a new todo..."
+                className={` w-full ${config.background} ${config.textColor} rounded-md py-6 pl-16 outline-none text-lg`} />
 
         </form>
     )
