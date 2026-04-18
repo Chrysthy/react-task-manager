979896f952c6eed43795732985208038b982b267
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-18 20:46:29 -0300

    fix: ajustar classes e estrutura do componente TodoList

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -9,49 +9,33 @@ const todos = [
 ]
 
 const TodoList = () => {
-
     const { theme } = useContext(ThemeContext);
     const config = themeConfig[theme].todo;
 
     return (
-
-        <div className={`${config.background} rounded-md`}>
+        <div className={`${config.background} border ${config.border} rounded-md`}>
             <ul>
                 {todos.map((todo) => (
-
-                    <li className={`${config.border} p-6`} key={todo.id}>
-
-                        <div className="flex item-center gap-4">
-
-                            <button className={`${config.textColor} w-6 h-6 border border-dark-text rounded-full cursor-pointer`}></button>
-
-                            <p className={`${config.textColor}`}>{todo.text}</p>
-
+                    <li className={`border-b ${config.border} p-6`} key={todo.id}>
+                        <div className="flex items-center gap-4">
+                            <button className={`w-6 h-6 border ${config.border} rounded-full cursor-pointer`}></button>
+                            <p className={config.textColor}>{todo.text}</p>
                         </div>
-
                     </li>
                 ))}
-
             </ul>
 
-            <div className={`flex items-center justify-between p-6 ${config.textColor} rounded-b-md`}>
-
+            <div className={`text-sm flex items-center justify-between p-6 ${config.textColor} rounded-b-md`}>
                 <p>{todos.length} items left</p>
 
                 <div className="flex gap-4">
-
-                    <button className={`${config.textColor} text-text-brighr-blue`}>All</button>
-
-                    <button className={`${config.textColor}`}>Active</button>
-
-                    <button className={`${config.textColor}`}>Completed</button>
-
+                    <button>All</button>
+                    <button className={config.textColor}>Active</button>
+                    <button className={config.textColor}>Completed</button>
                 </div>
 
-                <button className={`${config.textColor}`}>Clear Selected</button>
-
+                <button className={config.textColor}>Clear Selected</button>
             </div>
-
         </div>
     )
 }
