95ecbfd936260bda3a38420995079ce1f6c89df0
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-18 20:46:39 -0300

    feat: integrar contexto de tema e ajustar classes de layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import TodoHeader from "./components/TodoHeader";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { themeConfig } from './contexts/theme';
+import { ThemeContext } from './contexts/ThemeContext';
+import { useContext } from "react";
 
 
 function App() {
 
+  const { theme } = useContext(ThemeContext);
+  const config = themeConfig[theme];
+
   return (
     <>
-      <main className="bg-dark-bg h-screen">
+      <main className={`h-screen ${config.layout.background}`}>
 
-        <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+        <div className={`${config.layout.heroClass}`}>
 
           <div className="max-w-175 w-full m-auto p-8">
 
