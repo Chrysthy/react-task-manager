05127ba847b4211446cd8affed7501a051e5d18a
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-18 20:55:19 -0300

    fix: ajustar classes e estrutura do componente TodoHeader

--- src/components/TodoHeader/index.tsx
+++ src/components/TodoHeader/index.tsx
@@ -7,9 +7,9 @@ const TodoHeader = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
-        <header className="flex justify-between mb-6 pt-20">
+        <header className="flex justify-between mb-6 pt-20 items-center">
 
-            <h1 className="text-white text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
+            <h1 className="text-white text-4xl sm:text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
 
             <button className="cursor-pointer" onClick={toggleTheme}>
                 <img className="w-8 h-8" src={`${themeConfig[theme].icon}`} alt="Toggle Theme" />
