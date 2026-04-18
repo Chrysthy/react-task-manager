b41f5011fc00d5dea3d7e6585a882631d1abb143
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-18 20:55:28 -0300

    fix: ajustar estrutura do componente TodoList para melhor legibilidade

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -13,30 +13,44 @@ const TodoList = () => {
     const config = themeConfig[theme].todo;
 
     return (
-        <div className={`${config.background} border ${config.border} rounded-md`}>
-            <ul>
-                {todos.map((todo) => (
-                    <li className={`border-b ${config.border} p-6`} key={todo.id}>
-                        <div className="flex items-center gap-4">
-                            <button className={`w-6 h-6 border ${config.border} rounded-full cursor-pointer`}></button>
-                            <p className={config.textColor}>{todo.text}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+        <>
+            <div className={`${config.background} border ${config.border} rounded-md`}>
+                <ul>
+                    {todos.map((todo) => (
+                        <li className={`border-b ${config.border} p-6`} key={todo.id}>
+                            <div className="flex items-center gap-4">
+                                <button className={`w-6 h-6 border ${config.border} rounded-full cursor-pointer`}></button>
+                                <p className={config.textColor}>{todo.text}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-            <div className={`text-sm flex items-center justify-between p-6 ${config.textColor} rounded-b-md`}>
-                <p>{todos.length} items left</p>
+                <div className={`text-sm flex items-center justify-between p-6 ${config.textColor} rounded-b-md`}>
+                    <p>{todos.length} items left</p>
 
-                <div className="flex gap-4">
-                    <button>All</button>
-                    <button className={config.textColor}>Active</button>
-                    <button className={config.textColor}>Completed</button>
+                    <div className="hidden sm:flex gap-4">
+                        <button>All</button>
+                        <button className={config.textColor}>Active</button>
+                        <button className={config.textColor}>Completed</button>
+                    </div>
+
+                    <button className={config.textColor}>Clear Selected</button>
                 </div>
 
-                <button className={config.textColor}>Clear Selected</button>
+
+
             </div>
-        </div>
+
+            <div className={`flex justify-center gap-5 py-4 rounded-md mt-4 ${config.background} border ${config.border} rounded-md sm:hidden`}>
+
+                <button>All</button>
+                <button className={config.textColor}>Active</button>
+                <button className={config.textColor}>Completed</button>
+
+            </div>
+
+        </>
     )
 }
 
