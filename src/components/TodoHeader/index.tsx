55341da0e0c419157c6188dc28b1a7a118234a32
Chrystine Martins <chrystine.martins@outlook.com>
2026-04-18 20:20:36 -0300

    fix: corrigir erro de digitação na função de alternância de tema no TodoHeader

--- src/components/TodoHeader/index.tsx
+++ src/components/TodoHeader/index.tsx
@@ -4,14 +4,14 @@ import { useContext } from "react";
 
 const TodoHeader = () => {
 
-    const { theme, toogleTheme } = useContext(ThemeContext);
+    const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
         <header className="flex justify-between mb-6 pt-20">
 
             <h1 className="text-white text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
 
-            <button className="cursor-pointer" onClick={toogleTheme}>
+            <button className="cursor-pointer" onClick={toggleTheme}>
                 <img className="w-8 h-8" src={`${themeConfig[theme].icon}`} alt="Toggle Theme" />
             </button>
 
